ce81cb5a5c50d1becec2bc242474deeec63c7496
Atlan <gevaliatlaniskt@gmail.com>
2023-11-20 21:30:28 +0100

    added input component

--- frontend/src/components/input/Input.tsx
+++ frontend/src/components/input/Input.tsx
@@ -1,14 +1,29 @@
 import './Input.scss'
+import { Category, Subcategory } from '../../types/ProductTypes'
 
 type InputProps = {
   type: 'text' | 'number' | 'typeahead',
   placeholder: string,
+  label: string,
+  id: string,
+  data?: Category[] | Subcategory[]
 
 }
 
 function Input(props: InputProps) {
   if (props.type === 'typeahead')
     return (
-      <input type="text" />
+      <div>
+        <label htmlFor={props.id}>{props.label}</label>
+        <input id={props.id} type="text" placeholder={props.placeholder} />
+        <ul>
+          {props.data && props.data.map((item, _i) =>
+            <li key={item.title}>
+              {item.title}
+            </li>)}
+        </ul>
+      </div>
     )
 }
+
+export default Input
